7e296ae2fec83278795d1d5b79e4b72117e8be62
Marten <martenc@gmail.com>
2013-03-29 20:27:40 -0700

    small update

--- server.js
+++ server.js
@@ -32,68 +32,67 @@ app.configure(function () {
 
 var objToBeTracked;
 
-// if (objToBeTracked != undefined) {
-	// MongoDB - begin
-	/*
-	= 	  var dbserver = new mongo.Server(mongohost, mongoport, {w: 1});
-	=
-	=     w: (value of > -1 or the string 'majority'), where < 1 means                     =
-	=        no write acknowlegement                                                       =
-	=     journal: true/false, wait for flush to journal before acknowlegement             =
-	=     fsync: true/false, wait for flush to file system before acknowlegement           =   
+// MongoDB - begin
+/*
+= 	  var dbserver = new mongo.Server(mongohost, mongoport, {w: 1});
+=
+=     w: (value of > -1 or the string 'majority'), where < 1 means                     =
+=        no write acknowlegement                                                       =
+=     journal: true/false, wait for flush to journal before acknowlegement             =
+=     fsync: true/false, wait for flush to file system before acknowlegement           =   
 
-	=  For backward compatibility safe is still supported and                              =
-	=   allows values of [true | false | {j:true} | {w:n, wtimeout:n} | {fsync:true}]      =
-	=   the default value is false which means the driver receives does not                =
-	=   return the information of the success/error of the insert/update/remove            =
-	=                                                                                      =
-	=   ex: new Db(new Server('localhost', 27017), {safe:false})          
-	*/
-	var db = new mongo.Db(mongodbname, new mongo.Server(mongohost, mongoport, {}), {w: 1});
-	db.open(function(err, client){
-		// actually authenticating; often left out of examples
-		client.authenticate(mongousr, mongopwd, function(err, p_client) {
-			// good practice to create the collection if it doesn't exist 	
-		    client.createCollection("pixeltracking", function(err, col) {
-		         client.collection("pixeltracking", function(err, col) {
+=  For backward compatibility safe is still supported and                              =
+=   allows values of [true | false | {j:true} | {w:n, wtimeout:n} | {fsync:true}]      =
+=   the default value is false which means the driver receives does not                =
+=   return the information of the success/error of the insert/update/remove            =
+=                                                                                      =
+=   ex: new Db(new Server('localhost', 27017), {safe:false})          
+*/
+var db = new mongo.Db(mongodbname, new mongo.Server(mongohost, mongoport, {}), {w: 1});
+db.open(function(err, client){
+	// actually authenticating; often left out of examples
+	client.authenticate(mongousr, mongopwd, function(err, p_client) {
+		// good practice to create the collection if it doesn't exist 	
+	    client.createCollection("pixeltracking", function(err, col) {
+	         client.collection("pixeltracking", function(err, col) {
 
-		         	// leverage the awesome pixel-tracker module
-		         	tracker
-					  .use(function (error, result) {
-					    //objToBeTracked = JSON.stringify(result, null, 2);
-					    objToBeTracked = result;
+	         	// leverage the awesome pixel-tracker module
+	         	tracker
+				  .use(function (error, result) {
+				    //objToBeTracked = JSON.stringify(result, null, 2);
+				    objToBeTracked = result;
 
-					    // verify whats being captured
-					    console.log(objToBeTracked);
-					 	
-					 	/*
-					 	// mysql example should you wish to use it
-					 	connection.connect();
-						connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-						  if (err) throw err;
+				    // verify whats being captured
+				    console.log(objToBeTracked);
+				 	
+				 	/*
+				 	// mysql example should you wish to use it
+				 	connection.connect();
+					connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+					  if (err) throw err;
 
-						  console.log('The solution is: ', rows[0].solution);
-						});
-						connection.end();
-						*/
+					  console.log('The solution is: ', rows[0].solution);
+					});
+					connection.end();
+					*/
 
-						if (objToBeTracked != undefined) {
-							col.insert(objToBeTracked, function() {});
-						}
-						db.close();
-					  })
+					if (objToBeTracked != undefined) {
+						col.insert(objToBeTracked, function() {});
+					}
+					//db.close();
+				  })
+				  .configure({ disable_cookies : true }) // disabled 
 
 
-		             // if you wish to avoid inserting the above you can add three useless documents to test
-		             // for (var i = 0; i < 3; i++) {
-		             //    col.insert({c:i}, function() {});
-		             // }
-	
-		         });
-		    });
-		});
+	             // if you wish to avoid inserting the above you can add three useless documents to test
+	             // for (var i = 0; i < 3; i++) {
+	             //    col.insert({c:i}, function() {});
+	             // }
+
+	         });
+	    });
 	});
-// }
+});
 // MongoDB - end
 
 
